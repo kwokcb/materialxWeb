4a516dff182420aaa3660b02812d023a6320c3d1
Bernard Kwok <kwokcb@gmail.com>
2025-08-26 18:15:52 -0400

    Doc update.

--- documents/html/search/all_4.js
+++ documents/html/search/all_4.js
@@ -6,6 +6,5 @@ var searchData=
   ['downloadassetdatabase_3',['downloadAssetDatabase',['../class_ambient_c_g_loader.html#a38043b20adbfa70bc094c8be12db4eae',1,'AmbientCGLoader']]],
   ['downloadmaterialasset_4',['downloadMaterialAsset',['../class_ambient_c_g_loader.html#a2fd577bed5512051e624b625962c1ac7',1,'AmbientCGLoader']]],
   ['downloadmaterialslist_5',['downloadMaterialsList',['../class_ambient_c_g_loader.html#af763c2085ea7feeb80eb9ffd0da354e6',1,'AmbientCGLoader']]],
-  ['downloadpackage_6',['downloadPackage',['../class_js_g_p_u_open_material_loader.html#a17393d54017666b1a1d10b9e8a229477',1,'JsGPUOpenMaterialLoader']]],
-  ['downloadpackagebyexpression_7',['downloadPackageByExpression',['../class_js_g_p_u_open_material_loader.html#ac922c9c0fdd87552b5b43cf68455734c',1,'JsGPUOpenMaterialLoader']]]
+  ['downloadpackagebyexpression_6',['downloadPackageByExpression',['../class_js_g_p_u_open_material_loader.html#ac922c9c0fdd87552b5b43cf68455734c',1,'JsGPUOpenMaterialLoader']]]
 ];
--- documents/html/search/functions_4.js
+++ documents/html/search/functions_4.js
@@ -3,6 +3,5 @@ var searchData=
   ['downloadassetdatabase_0',['downloadAssetDatabase',['../class_ambient_c_g_loader.html#a38043b20adbfa70bc094c8be12db4eae',1,'AmbientCGLoader']]],
   ['downloadmaterialasset_1',['downloadMaterialAsset',['../class_ambient_c_g_loader.html#a2fd577bed5512051e624b625962c1ac7',1,'AmbientCGLoader']]],
   ['downloadmaterialslist_2',['downloadMaterialsList',['../class_ambient_c_g_loader.html#af763c2085ea7feeb80eb9ffd0da354e6',1,'AmbientCGLoader']]],
-  ['downloadpackage_3',['downloadPackage',['../class_js_g_p_u_open_material_loader.html#a17393d54017666b1a1d10b9e8a229477',1,'JsGPUOpenMaterialLoader']]],
-  ['downloadpackagebyexpression_4',['downloadPackageByExpression',['../class_js_g_p_u_open_material_loader.html#ac922c9c0fdd87552b5b43cf68455734c',1,'JsGPUOpenMaterialLoader']]]
+  ['downloadpackagebyexpression_3',['downloadPackageByExpression',['../class_js_g_p_u_open_material_loader.html#ac922c9c0fdd87552b5b43cf68455734c',1,'JsGPUOpenMaterialLoader']]]
 ];
